769ea301941b147c96286604403531596c54cad8
jake <jakeoleary96@gmail.com>
2023-08-01 10:35:29 -0700

    when dragging a piece, only the piece svg drags now. All squares are now droppable. Dropping a piece currently does nothing though.

--- client/src/components/ActiveGame.tsx
+++ client/src/components/ActiveGame.tsx
@@ -3,9 +3,10 @@ import { Piece, grid, process_move } from '../utils/game-utils'
 import { GameContext } from "../context/GameContext";
 import { ConnectableElement, useDrag, useDrop } from 'react-dnd';
 import { svgIcons } from '../utils/svg-icons';
+import { HTML } from 'react-dnd-html5-backend/dist/NativeTypes';
 
 interface DraggablePieceProps {
-  spot_piece: Piece;
+  spot_piece: Piece | null;
   squareColor: string;
 }
 
@@ -52,27 +53,15 @@ const ChessBoard: React.FC<{}> = () => {
 
   for (let row_num = 0; row_num < 8; row_num++) {
     let row = [];
-    console.log(`building board first for loop firing ${row_num}`)
     for (let col_num = 0; col_num < 8; col_num++) {
-      console.log(`building board second for loop firing ${col_num}`)
       if (gameState) {
-        console.log(`in chessboard building board firing if gameState exists ${col_num}`)
-        console.log("gameState in chessboard building the board VVVVVVV")
-        console.log(gameState)
         let spot_piece = gameState[grid[col_num][row_num]][0];
         let squareColor = row_num % 2 === 0 
         ? col_num % 2 === 0 ? 'bg-black-square' : 'bg-white-square' 
         : col_num % 2 === 0 ? 'bg-white-square' : 'bg-black-square';
-        console.log("in building the board right before check for spot_piece exists spot_piece is VVVVVVVVVVVVVVVVVV")
-        console.log(spot_piece)
-        if (spot_piece){
-          console.log(`in building board firing if spot_piece exists ${col_num}`)
-          row.push(
-            <DraggablePiece key={`${row_num}-${col_num}`} spot_piece={spot_piece} squareColor={squareColor} />
-          );
-        } else {
-          row.push(<div key={`${row_num}-${col_num}`} className={`w-square h-square flex items-center justify-center ${squareColor}`}></div>)
-        }
+        row.push(
+          <DraggablePiece key={`${row_num}-${col_num}`} spot_piece={spot_piece} squareColor={squareColor} />
+        );
       }
     }
     chessBoard.push(<div key={row_num} className="w-screen flex items-center justify-center">{row}</div>);
@@ -88,9 +77,6 @@ const ChessBoard: React.FC<{}> = () => {
 const DraggablePiece: React.FC<DraggablePieceProps> = ({ spot_piece, squareColor }) => {
 
   const{ initiatingUser: player1, receivingUser: player2, setInitiatingUser, setReceivingUser, gameState, setGameState } = useContext(GameContext);
-  const piece = svgIcons[spot_piece.pieceName];
-  console.log(`draggablepiece being rendered the piece(supposed to be svg) isvvv`)
-  console.log(piece)
 
   const [{ isDragging }, dragRef] = useDrag({
     type: 'piece',
@@ -103,18 +89,20 @@ const DraggablePiece: React.FC<DraggablePieceProps> = ({ spot_piece, squareColor
   const [{ canDrop, isOver }, dropRef] = useDrop({
     accept: 'piece',
     drop: (item: any, monitor) => {
-      const start = item.piece.position; 
-      const end = spot_piece.position;
-      let copy_state = {...gameState};
-      if (player1 && player2) {
-        const moveResult = process_move(start, end, copy_state, player1, player2 )
-        if (moveResult.isValid) {
-          setInitiatingUser(moveResult.player1);
-          setReceivingUser(moveResult.player2);
-          setGameState(moveResult.newState);
-        }
-        else {
-          setGameState(moveResult.newState);
+      if (spot_piece) {
+        const start = item.piece.position; 
+        const end = spot_piece.position;
+        let copy_state = {...gameState};
+        if (player1 && player2) {
+          const moveResult = process_move(start, end, copy_state, player1, player2 )
+          if (moveResult.isValid) {
+            setInitiatingUser(moveResult.player1);
+            setReceivingUser(moveResult.player2);
+            setGameState(moveResult.newState);
+          }
+          else {
+            setGameState(moveResult.newState);
+          }
         }
       }
     },
@@ -124,15 +112,13 @@ const DraggablePiece: React.FC<DraggablePieceProps> = ({ spot_piece, squareColor
     }),
   })
 
-  // Compose drag and drop refs
-  const combinedRef = (node: ConnectableElement) => {
-    dragRef(node)
-    dropRef(node)
-  }
-
   return (
-    <div ref={combinedRef} className={`w-square h-square flex items-center justify-center ${squareColor}`}>
-      {piece}
-    </div>
+    spot_piece ? (
+      <div ref={dropRef} className={`w-square h-square flex items-center justify-center ${squareColor}`}>
+        <div ref={dragRef}>{svgIcons[spot_piece.pieceName]}</div>
+      </div>
+    ) : (
+      <div ref={dropRef} className={`w-square h-square flex items-center justify-center ${squareColor}`}></div>
+    )
   )
 }
